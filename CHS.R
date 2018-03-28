9becfce68d8aa4855d633801759713ce59836a93
barryguglielmo <barryguglielmo@gmail.com>
2018-03-28 15:45:30 -0400

    Update CHS.R

--- CHS.R
+++ CHS.R
@@ -7,18 +7,21 @@ if (!require("RColorBrewer")) {
   library(RColorBrewer)
 }
 
-#set wd
-setwd("C:/Users/bag019/Desktop/R Programs/GEMS")
+
 #read in csv file and get data
-data <- read.csv("CHS - Z.csv")
+my_data <- read.csv(file.choose(), header = T)
 #set the color palette
 my_palette <- colorRampPalette(c( "white", "red" ))(n = 142)
+#select data (row, col)
+selected_data <- my_data[c(2:142), c(2:156)]
 #data to matrix                 [row , col]
-othermatrix <- na.pass(data.matrix(data))
+my_matrix <- data.matrix(selected_data)
 #z score: z = x-mean/stdev
-heatmap.2( othermatrix,
-           labRow = data[,1],
+#https://stat.ethz.ch/R-manual/R-devel/library/stats/html/heatmap.html
+heatmap.2( my_matrix,
+           Colv = FALSE,
+           labRow = my_data[,1],
            main = "Double Normilization to 100 and Zscore", # heat map title
-           trace="none",         # turns off trace lines inside the heat map
-           margins =c(12,9),     # widens margins around plot
-           col=my_palette)       # use on color palette defined earlier
+           trace="none",                                    # turns off trace lines inside the heat map
+           margins =c(12,9),                                # widens margins around plot
+           col=my_palette)       
