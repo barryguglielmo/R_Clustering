ce62d269699e9190547e8a7bf374d0164ec50907
barryguglielmo <barryguglielmo@gmail.com>
2018-03-28 15:07:53 -0400

    Update Hexbin.R

--- Hexbin.R
+++ Hexbin.R
@@ -5,5 +5,7 @@ data <- read.csv(file.choose(), header = T)
 #set x and y (column 1 is x's column 2 is y's)
 x <- data[[1]]
 y <- data[[2]]
-#Plot it
-plot(hexbin(x, y))
+#make the bin as variable
+my_bin <-hexbin(x, y)
+#Plot it (BTY, terrain.colors, rainbow)
+plot(my_bin, colramp= BTY)
