f74adedc19a3afe22e00293b90d2d5621ff66f9c
barryguglielmo <barryguglielmo@gmail.com>
2018-03-28 14:49:33 -0400

    Update Hexbin.R

--- Hexbin.R
+++ Hexbin.R
@@ -1,4 +1,9 @@
-data <- read.csv(file.choose(), header = T)
-set.seed(101)
+#Get Hexbin Library
 library(hexbin)
-plot(hexbin(data$WP.AI, data$WP.HX))
+#read csv file
+data <- read.csv(file.choose(), header = T)
+#set x and y (column 1 is x's column 2 is y's)
+x <- data[[1]]
+y <- data[[2]]
+#Plot it
+plot(hexbin(x, y))
